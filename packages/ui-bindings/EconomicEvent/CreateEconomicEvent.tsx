628d6221cfc6fb0f3ba230165af970e39bc31aae
Connor Hibbs <hibbscm@msoe.edu>
2018-02-05 10:49:27 -0600

    Worked through getting branch running to test out Chrome extension

--- packages/ui-bindings/EconomicEvent/CreateEconomicEvent.tsx
+++ packages/ui-bindings/EconomicEvent/CreateEconomicEvent.tsx
@@ -58,25 +58,38 @@ const mutation = gql`
   }
 `;
 
-let connectFunction = connect(state => ({
-  variables: {
-    token: getActiveLoginToken(state)
-  }
-}));
-
-let graphQLQueryOptions = (props) => (console.log("Received", props, "for the mutation"), {
-  variables: {
-    ...props.variables,
-    ...props.data
-  },
-});
-
-let graphQLQueryProps = (ownProps, { data: { createEconomicEvent, loading, error } }) =>
-  (console.log("Inside props", error), {
-    economicEvent: createEconomicEvent ? createEconomicEvent.economicEvent : null,
-    loading,
-    error
-  });
+// let connectFunction = connect(state => ({
+//   variables: {
+//     token: getActiveLoginToken(state)
+//   }
+// }));
+//
+// let graphQLQueryOptions = (props) => (console.log("Received", props, "for the mutation"), {
+//   variables: {
+//     ...props.variables,
+//     ...props.data
+//   },
+// });
+//
+// let graphQLQueryProps = (ownProps, { data: { createEconomicEvent, loading, error } }) =>
+//   (console.log("Inside props", error), {
+//     economicEvent: createEconomicEvent ? createEconomicEvent.economicEvent : null,
+//     loading,
+//     error
+//   });
+//
+// export default compose(
+//   connect(state => ({
+//     variables: {
+//       token: getActiveLoginToken(state)
+//     }
+//   })),
+//
+//   graphql(mutation, {
+//     options: graphQLQueryOptions,
+//     props: graphQLQueryProps,
+//   })
+// );
 
 export default compose(
   connect(state => ({
@@ -86,39 +99,25 @@ export default compose(
   })),
 
   graphql(mutation, {
-    options: graphQLQueryOptions,
-    props: graphQLQueryProps,
+    options: (props) => (console.log("Received", props, "for the mutation"), {
+      variables: {
+        ...props.variables,
+        ...props.data
+      },
+    }),
+  // }), // TODO delete
+    props:
+      ({
+         data: {
+           createEconomicEvent,
+           loading,
+           error
+         }
+       }) =>
+        (console.log("Inside props", error), {
+          // economicEvent: createEconomicEvent ? createEconomicEvent.economicEvent : null,
+          // loading,
+          error
+        }),
   })
 );
-
-
-// export default compose(
-//   connect(state => ({
-//     variables: {
-//       token: getActiveLoginToken(state)
-//     }
-//   })),
-//
-//   graphql(mutation, {
-//     options: (props) => (console.log("Received", props, "for the mutation"), {
-//       variables: {
-//         ...props.variables,
-//         ...props.data
-//       },
-//     }),
-//   // }), // TODO delete
-//     props:
-//       ({
-//          data: {
-//            createEconomicEvent,
-//            loading,
-//            error
-//          }
-//        }) =>
-//         (console.log("Inside props", error), {
-//           // economicEvent: createEconomicEvent ? createEconomicEvent.economicEvent : null,
-//           // loading,
-//           error
-//         }),
-//   })
-// );
