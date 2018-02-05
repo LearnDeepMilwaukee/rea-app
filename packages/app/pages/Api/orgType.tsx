c749b41bee96dc2c9a72a065f44316e0bdd99b1a
Ryan_Guinn <guinnrd@msoe.edu>
2018-02-05 10:23:00 -0600

    Removed individual querey functionality

--- packages/app/pages/Api/orgType.tsx
+++ packages/app/pages/Api/orgType.tsx
@@ -10,7 +10,6 @@
 import * as React from "react";
 import "./api.css"
 import GetOrganizationTypes from "../../../ui-bindings/OrganizationType/getAllOrganizationTypes.js";
-import GetOrganizationType from "../../../ui-bindings/OrganizationType/getOrganizationTypeById.js"
 
 export const OrganizationType = (props) => {
   return(
@@ -18,17 +17,6 @@ export const OrganizationType = (props) => {
       <div>id: {props.id}</div>
       <div>name: {props.name}</div>
       <br/>
-    </div>
-  );
-};
-
-const OrganizationTypeField = (props) => {
-  return(
-    <div>
-      <form onSubmit={props.onSubmitAction}>
-        Enter an Id: <input type="text" name="value" onChange={props.setOrganizationType}/>
-        <input type="submit" value="query"/>
-      </form>
     </div>
   );
 };
@@ -45,53 +33,13 @@ export const OrganizationTypeList = GetOrganizationTypes(({ orgTypeList, loading
   );
 });
 
-export const GetSingleOrganizationType = GetOrganizationType(
-  (
-    { orgType, loading, error }
-  ) => {
-    return (
-      loading ? <strong>Loading...</strong> : (
-        error ? <p style={{color: "#F00"}}>API error</p> : (
-          <div>
-            <OrganizationType id={orgType.id} name={orgType.name}/>
-          </div>
-        )
-      )
-    );
-  }
-);
-
 class App extends React.Component {
-
-  state = {
-    getOneOrganizationTypeId: null,
-    setOneOrganizationTypeId: null
-  };
-
-  // Runs every time the input field changes
-  getOrganizationTypeById = (event) => {
-    this.setState({setOneOrganizationTypeId: parseInt(event.target.value, 10)});
-  };
-
-  // Runs when "submit" is selected
-  stopRefresh = (event) => {
-    // Sets the value to query to the current value of the input field
-    this.setState({getOneOrganizationTypeId: this.state.setOneOrganizationTypeId});
-    event.preventDefault();
-  };
-
   render() {
-    const {getOneOrganizationTypeId} = this.state;
     return (
       <div>
         <h2>List of all organizationTypes: </h2>
         <br/>
         <OrganizationTypeList/>
-        <br/>
-        <h2>Get OrganizationType by Id: </h2>
-        <br/>
-        <OrganizationTypeField setOrganizationType={this.getOrganizationTypeById} onSubmitAction={this.stopRefresh}/>
-        {getOneOrganizationTypeId ? <GetSingleOrganizationType orgTypeId={getOneOrganizationTypeId}/> : <div>Enter a value</div>}
       </div>
     );
   }
