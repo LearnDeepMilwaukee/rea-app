7355dcb601e1cd753464f50171c95ae5476a541e
Nicholas Roth <lou3797@gmail.com>
2018-02-12 09:08:40 -0600

    Basic fields added

--- packages/ui-bindings/person/personInterface.tsx
+++ packages/ui-bindings/person/personInterface.tsx
@@ -5,3 +5,42 @@
  * @author Nicholas Roth
  * @since 2018-2-9
  */
+
+import { gql } from "react-apollo";
+
+/**
+ * GraphQL Query fragment for a Person
+ */
+export const processClassificationInterface = gql`
+fragment processClassificationInterface on ProcessClassification {
+  id
+  name
+  type
+  image
+  note
+  ownedEconomicResources
+  agentProcesses
+  agentPlan
+  agentEconomicEvents
+  agentCommitments
+  agentRelationships
+  agentRoles
+  agentRecipes
+}`;
+
+/**
+ * Object type specifications
+ */
+export interface ProcessClassification {
+  id: number
+  name: String
+  note: String
+  ownedEconomicResources: String
+  agentProcesses: String
+  agentPlan: String
+  agentEconomicEvents: String
+  agentCommitments: String
+  agentRelationships: String
+  agentRoles: String
+  agentRecipes: String
+}
