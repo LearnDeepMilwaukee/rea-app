5e89bbccdf51c0fa9c782e6f7f2f4ffaa7930989
Steven Fontaine <fontainesw@msoe.edu>
2018-02-15 10:27:26 -0600

    var >> let

--- packages/app/pages/Api/common.tsx
+++ packages/app/pages/Api/common.tsx
@@ -5,12 +5,12 @@
  * @returns {string}
  */
 export function concatArray(array: JSON[]) {
-  var retValue = "";
+  let retValue = "";
   if (array[0] == null) {
     return "none";
   }
   retValue = array[0].id;
-  for (var i = 1; i < array.length; i++) {
+  for (let i = 1; i < array.length; i++) {
     retValue += ", ";
     retValue += array[i].id;
   }
