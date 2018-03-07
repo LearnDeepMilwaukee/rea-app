49e5c3471cda85f87d06809d40f0707649f7cc6f
Steven Fontaine <fontainesw@msoe.edu>
2018-03-07 10:07:20 -0600

    Started cleaning up landing to make it more understandable

--- packages/app/pages/Landing/landing.tsx
+++ packages/app/pages/Landing/landing.tsx
@@ -30,19 +30,20 @@ interface UserProps {
   },
 }
 
-const Theme = () =>{
-  let currentTheme = themeable(theme);
+const Theme = (props) =>{
+  let currentTheme = themeable(props.theme);
   return(
     <section {...currentTheme(1, 'landing')}>
       <div {...currentTheme(2, 'row')}>
         <div {...currentTheme(3, 'medium-5', 'medium-centered', 'columns')} >
           <div {...currentTheme(1000, 'landingContent')}>
+            {props.child}
           </div>
         </div>
       </div>
     </section>
   )
-}
+};
 
 const YourProjects = CurrentUser(withRouter(({ user, loading, error, theme, router }: UserProps) => {
   let currentTheme = themeable(theme);
@@ -60,24 +61,22 @@ const YourProjects = CurrentUser(withRouter(({ user, loading, error, theme, rout
   else {
 
     return (
-      <Theme>
-        <h1>Hello, {user.name} 👋</h1>
-        <div {...currentTheme(4, 'project_list')}>
-          <h5>Choose one of the projects of your network</h5>
-          <ul {...currentTheme(30000, 'aside_list')} >
-            {user.agentRelationships.map((item, i) => (
-              <li {...currentTheme((3 * i) + 1, 'list_item', router.isActive('projects/' + item.object.id) && 'active')} >
-                <Link href={'projects/' + item.object.id} {...currentTheme((3 * i) + 2, 'item_link')} >
-                        <span {...currentTheme((3 * i) + 3, 'link_image')}>
-                          <img src={item.object.image}/>
-                        </span>
-                  <h4>{item.object.name}</h4>
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </Theme>
+      <Theme theme={theme} child={<h1>Hello, {user.name} 👋</h1>
+      <div {...currentTheme(4, 'project_list')}>
+        <h5>Choose one of the projects of your network</h5>
+        <ul {...currentTheme(30000, 'aside_list')} >
+        {user.agentRelationships.map((item, i) => (
+          <li {...currentTheme((3 * i) + 1, 'list_item', router.isActive('projects/' + item.object.id) && 'active')} >
+            <Link href={'projects/' + item.object.id} {...currentTheme((3 * i) + 2, 'item_link')} >
+                <span {...currentTheme((3 * i) + 3, 'link_image')}>
+                  <img src={item.object.image}/>
+                </span>
+              <h4>{item.object.name}</h4>
+            </Link>
+          </li>
+        ))}
+        </ul>
+        </div>}/>
     );
   }
 
