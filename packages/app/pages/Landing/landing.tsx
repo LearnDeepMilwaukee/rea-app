378ad962b10d404ea4549eb96236985bfdea75f4
Steven Fontaine <fontainesw@msoe.edu>
2018-03-07 09:44:34 -0600

    Started cleaning up landing to make it more understandable

--- packages/app/pages/Landing/landing.tsx
+++ packages/app/pages/Landing/landing.tsx
@@ -30,36 +30,57 @@ interface UserProps {
   },
 }
 
-const YourProjects = CurrentUser(withRouter(({ user, loading, error, theme, router }: UserProps) => {
-    let currentTheme = themeable(theme)
-    return (
-        loading ? <strong>Loading...</strong> : (error ? <p style={{ color: '#F00' }}>API error</p> : (
-        <section {...currentTheme(1, 'landing')}>
-          <div {...currentTheme(2, 'row')}>
-            <div {...currentTheme(3, 'medium-5', 'medium-centered', 'columns')} >
-              <div {...currentTheme(1000, 'landingContent')}>
-                <h1>Hello, {user.name} 👋</h1>
-                <div {...currentTheme(4, 'project_list')}>
-                  <h5>Choose one of the projects of your network</h5>
-                  <ul {...currentTheme(30000, 'aside_list')} >
-                    {user.agentRelationships.map((item, i) => (
-                    <li {...currentTheme(i + i + i + 1, 'list_item', router.isActive('projects/' + item.object.id) && 'active')} >
-                        <Link href={'projects/' + item.object.id} {...currentTheme(i + i + i + 2, 'item_link')} >
-                            <span {...currentTheme( i + i + i + 3, 'link_image')}>
-                                <img src={item.object.image} />
-                            </span>
-                            <h4>{item.object.name}</h4>
-                        </Link>
-                    </li>
-                  ))}
-                  </ul>
-                </div>
-              </div>
-            </div>
+const Theme = () =>{
+  let currentTheme = themeable(theme);
+  return(
+    <section {...currentTheme(1, 'landing')}>
+      <div {...currentTheme(2, 'row')}>
+        <div {...currentTheme(3, 'medium-5', 'medium-centered', 'columns')} >
+          <div {...currentTheme(1000, 'landingContent')}>
           </div>
-        </section>
-      )
-    ))
-}))
+        </div>
+      </div>
+    </section>
+  )
+}
+
+const YourProjects = CurrentUser(withRouter(({ user, loading, error, theme, router }: UserProps) => {
+  let currentTheme = themeable(theme);
+
+  if(loading){
+    return(
+      <strong>Loading...</strong>
+    )
+  }
+  else if(error){
+    return(
+      <p style={{ color: '#F00' }}>API error</p>
+    )
+  }
+  else {
+
+    return (
+      <Theme>
+        <h1>Hello, {user.name} 👋</h1>
+        <div {...currentTheme(4, 'project_list')}>
+          <h5>Choose one of the projects of your network</h5>
+          <ul {...currentTheme(30000, 'aside_list')} >
+            {user.agentRelationships.map((item, i) => (
+              <li {...currentTheme((3 * i) + 1, 'list_item', router.isActive('projects/' + item.object.id) && 'active')} >
+                <Link href={'projects/' + item.object.id} {...currentTheme((3 * i) + 2, 'item_link')} >
+                        <span {...currentTheme((3 * i) + 3, 'link_image')}>
+                          <img src={item.object.image}/>
+                        </span>
+                  <h4>{item.object.name}</h4>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </Theme>
+    );
+  }
+
+}));
 
 export default YourProjects
