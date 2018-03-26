82285159cc37e2f246c05085342ed6edb2ddef4a
Steven Fontaine <fontainesw@msoe.edu>
2018-03-25 20:36:32 -0500

    Fixed Linter errors in landing

--- packages/app/pages/Landing/landing.tsx
+++ packages/app/pages/Landing/landing.tsx
@@ -33,30 +33,28 @@ interface UserProps {
 const YourProjects = CurrentUser(withRouter(({ user, loading, error, theme, router }: UserProps) => {
     let currentTheme = themeable(theme);
 
-    if(loading){
+    if (loading) {
       return(
         <strong>Loading...</strong>
       );
-    }
-    else if(error){
+    } else if (error) {
       return(
-        <p style={{ color: '#F00' }}>API error</p>
+        <p style={{ color: "#F00" }}>API error</p>
       );
-    }
-    else{
+    } else {
       return(
-        <section {...currentTheme(1, 'landing')}>
-          <div {...currentTheme(2, 'row')}>
-            <div {...currentTheme(3, 'medium-5', 'medium-centered', 'columns')} >
-              <div {...currentTheme(1000, 'landingContent')}>
+        <section {...currentTheme(1, "landing")}>
+          <div {...currentTheme(2, "row")}>
+            <div {...currentTheme(3, "medium-5", "medium-centered", "columns")} >
+              <div {...currentTheme(1000, "landingContent")}>
                 <h1>Hello, {user.name} 👋</h1>
-                <div {...currentTheme(4, 'project_list')}>
+                <div {...currentTheme(4, "project_list")}>
                   <h5>Choose one of the projects of your network</h5>
-                  <ul {...currentTheme(30000, 'aside_list')} >
+                  <ul {...currentTheme(30000, "aside_list")} >
                     {user.agentRelationships.map((item, i) => (
-                      <li {...currentTheme(i + i + i + 1, 'list_item', router.isActive('projects/' + item.object.id) && 'active')} >
-                        <Link href={'projects/' + item.object.id} {...currentTheme(i + i + i + 2, 'item_link')} >
-                            <span {...currentTheme( i + i + i + 3, 'link_image')}>
+                      <li{...currentTheme(i + i + i + 1, "list_item", router.isActive("projects/" + item.object.id) && "active")} >
+                        <Link href={"projects/" + item.object.id} {...currentTheme(i + i + i + 2, "item_link")} >
+                            <span {...currentTheme( i + i + i + 3, "link_image")}>
                                 <img src={item.object.image} />
                             </span>
                           <h4>{item.object.name}</h4>
