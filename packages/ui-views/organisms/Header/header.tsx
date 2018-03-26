f4c089f803343031711e907ed5523e470631fe19
Steven Fontaine <fontainesw@msoe.edu>
2018-03-25 20:18:24 -0500

    Finished the dropdown menu. it now displays links in a dropdown with the same background color as the header. The only link I have in there so far is for "/" as "home"

--- packages/ui-views/organisms/Header/header.tsx
+++ packages/ui-views/organisms/Header/header.tsx
@@ -1,9 +1,10 @@
 import * as React from "react";
+import { Link } from "react-router";
 import * as themeable from "react-themeable";
 import { SFC } from "react";
 import CurrentUser from "@vflows/bindings/user/CurrentUser";
 import Link from "../../atoms/Link";
-import {Bell, Search, Horizontal} from "../../icons";
+import { Bell, Search, Horizontal } from "../../icons";
 import Dropdown from "./dropdownMenu";
 
 interface UserProps {
@@ -19,29 +20,36 @@ interface UserProps {
 const Header = CurrentUser(({ user, loading, error, theme }: UserProps) => {
     let currentTheme = themeable(theme);
     return (
-        loading ? <strong>Loading...</strong> : (error ? <p style={{ color: '#F00' }}>API error</p> : (
-        <header {...currentTheme(0, 'main_header')} >
-          <div {...currentTheme(1, 'row')}>
-            <Link href='/'><span  {...currentTheme(2, 'header_logo')} /></Link>
-            <div {...currentTheme(4, 'header_search')} >
-              <input {...currentTheme(5, 'search', 'input')} placeholder='Search' />
-              <span {...currentTheme(6, 'search_icon')}><Search /></span>
+        loading ? <strong>Loading...</strong> : (error ? <p style={{ color: "#F00" }}>API error</p> : (
+        <header {...currentTheme(0, "main_header")} >
+          <div {...currentTheme(1, "row")}>
+            <Link href="/"><span  {...currentTheme(2, "header_logo")} /></Link>
+            <div {...currentTheme(4, "header_search")} >
+              <input {...currentTheme(5, "search", "input")} placeholder="Search" />
+              <span {...currentTheme(6, "search_icon")}><Search /></span>
             </div>
-            <div {...currentTheme(3, 'header_menu')} >
-                  <div {...currentTheme(17, 'menu_bell')}>
+            <div {...currentTheme(3, "header_menu")} >
+                  <div {...currentTheme(17, "menu_bell")}>
                     <Bell />
                   </div>
-                  <div {...currentTheme(14, 'menu_profile')} >
-                    <div {...currentTheme(15, 'profile_image')}>
+                  <div {...currentTheme(14, "menu_profile")} >
+                    <div {...currentTheme(15, "profile_image")}>
                       <img src={user.image} />
                     </div>
-                    <h4>{user.name || 'nobody'}</h4>
-                    <span {...currentTheme(16, 'profile_other')}><Dropdown/></span>
+                    <h4>{user.name || "nobody"}</h4>
+                    <span {...currentTheme(16, "profile_other")}>
+                      <div {...currentTheme(17, "dropdown")}>
+                        <span>°°°</span>
+                        <div {...currentTheme(18, "dropdown-content")}>
+                          <Link to={"/"} {...currentTheme(19, "link")}>home</Link>
+                        </div>
+                      </div>
+                    </span>
                 </div>
             </div>
-            <div {...currentTheme(284484, 'header_mobile')}>
-              <ul {...currentTheme(24449, 'mobile_list')}>
-                <li {...currentTheme(22383, 'active')}>Projects</li>
+            <div {...currentTheme(284484, "header_mobile")}>
+              <ul {...currentTheme(24449, "mobile_list")}>
+                <li {...currentTheme(22383, "active")}>Projects</li>
                 <li>Activities</li>
                 <li>Profile</li>
                 <li>Notifications</li>
@@ -50,7 +58,7 @@ const Header = CurrentUser(({ user, loading, error, theme }: UserProps) => {
           </div>
         </header>
       )
-    ))
-})
+    ));
+});
 
-export default Header
+export default Header;
