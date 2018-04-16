babd59ed526ac53d496d621fd320a711b7305057
Connor Hibbs <hibbscm@msoe.edu>
2018-04-15 21:16:48 -0500

    Added password validation and new fields for user org. Fixed problems with constant fields

--- packages/app/pages/Registration/registration.tsx
+++ packages/app/pages/Registration/registration.tsx
@@ -1,59 +1,110 @@
 import * as React from "react";
 
+class PasswordSection extends React.Component {
+
+  state = {
+    passwordValid: true,
+    passwordsMatch: true
+  };
+
+  checkPasswordValid = () => {
+    let password = document.getElementById("password").value;
+
+    if (password !== "") {
+      this.setState({passwordValid: password.length >= 8});
+    }
+  };
+
+  checkPasswordsMatch = () => {
+    let password = document.getElementById("password").value;
+    let confirmPassword = document.getElementById("confirmPassword").value;
+
+    if (confirmPassword !== "") {
+      this.setState({passwordsMatch: (password === confirmPassword)});
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        Password:<br/>
+        <input
+          id="password"
+          type="password"
+          onChange={() => {
+            this.checkPasswordValid();
+            this.checkPasswordsMatch();
+          }}
+        />
+        {!this.state.passwordValid ? <p>Passwords Not Long Enough</p> : null}
+        <br/><br/>
+
+        Confirm Password:<br/>
+        <input id="confirmPassword" type="password" onChange={this.checkPasswordsMatch}/>
+        {!this.state.passwordsMatch ? <p>Passwords Do Not Match</p> : null}
+        <br/><br/>
+      </div>
+    );
+  }
+}
+
 class Registration extends React.Component {
 
-  passwordsMatch = true;
-
-  static getRegistrationJSON(event) {
+  getRegistrationJSON = (event) => {
     event.preventDefault();
 
+    // let allValid = this.state.passwordValid && this.state.passwordsMatch;
+    //
+    // if (!allValid) {
+    //   alert("Please correct information and try again");
+    //   return;
+    // }
 
+    let userType = document.getElementById("organizationButton").checked ? "organization" : "individual";
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").getPassword();
 
-    let userTypeRadioGroup = document.getElementById("userType");
-    let userType = userTypeRadioGroup.selected.label.toLowerCase();
     console.log("User Type:", userType);
-  }
-
-  checkPasswords() {
-    //TODO check if the passwords are the same or not, and update the condition
-    this.passwordsMatch = false;
-  }
+    console.log("Username:", username);
+    console.log("Password:", password);
+  };
 
   render() {
     return (
       <div>
         <h1>Register</h1>
 
-        <form id="form" onSubmit={Registration.getRegistrationJSON}>
+        <form id="form" onSubmit={this.getRegistrationJSON}>
 
-          User Type:
-          {/*<radiogroup id="userType">*/}
-            {/*<radio label="Organization" selected={true}/>*/}
-            {/*<radio label="Individual" disabled={true}/>*/}
-          {/*</radiogroup>*/}
-
-          <input type="radio" name="User Type" value="organization" selected={true}/>Organization
-          <input type="radio" name="User Type" value="individual" disabled={true}/>Individual
+          User Type:<br/>
+          <input
+            id="organizationButton"
+            type="radio"
+            name="userType"
+            value="organization"
+            defaultChecked="true"
+          />Organization
+          <input
+            id="individualButton"
+            type="radio"
+            name="userType"
+            value="individual"
+            disabled="true"
+          />Individual
           <br/><br/>
 
-          Username:
-          <input name="username" type="text" defaultValue=""/>
+          Organization Name:<br/>
+          <input id="organizationName" type="text"/>
           <br/><br/>
 
-          Password:
-          <input name="password" type="text" defaultValue=""/>
+          Username:<br/>
+          <input id="username" type="text"/>
           <br/><br/>
 
-          Confirm Password:
-          <input name="createResource" type="text" defaultValue=""/>
-          {this.passwordsMatch ? null : <p>Passwords Do Not Match</p>}
-          <br/><br/>
+          <PasswordSection id="password"/>
 
-
-          <input type="submit" id="submit" value="Create Economic Event"/>
+          <input type="submit" id="submit" value="Register"/>
         </form>
-
-        {/*{this.state.economicEvent ? <SingleEconomicEvent economicEvent={this.state.economicEvent} /> : <p>Created Event Goes Here</p>}*/}
       </div>
     );
   }
