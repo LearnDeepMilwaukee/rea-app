afe2a64893c2e2f3743b351b88a7666f4666bbc3
Nicholas Roth <lou3797@gmail.com>
2018-04-16 10:01:30 -0500

    Improper props access added. Need to actually query data for a specific Organization.

--- packages/app/pages/ModifyOrganization/modifyOrg.tsx
+++ packages/app/pages/ModifyOrganization/modifyOrg.tsx
@@ -5,6 +5,25 @@ import InventoryModal from "../../../ui-views/organisms/InventoryModal";
 import * as _ from "underscore";
 import getOrganizationById from "../../../ui-bindings/Organization/getOrganizationById";
 
+export const GetSingleOrganization = getOrganizationById(({ organization, loading, error }) => {
+
+  if (loading) {
+    return(
+      <strong>Loading...</strong>
+    );
+  } else if (error) {
+    return(
+      <p style={{color: "#F00"}}>API error</p>
+    );
+  } else {
+    return(
+      <div>
+        <Organization organization={organization}/>
+      </div>
+    );
+  }
+});
+
 /**
  * The inventory page, which contains the basic page structure
  * (menu, sidebar, sub-menus, and the entire inventory list)
@@ -23,13 +42,27 @@ class Inventory extends React.Component {
     this.theme = props.theme;
   }
 
+
+
   render() {
     let currentTheme = themeable(this.theme);
+    let org = this.props.organization;
+    let name = org.name;
+    let type = org.type;
+    let image = org.image;
+    let note = org.note;
 
     return (
       <div>
         <h4>
-          Test
+          <div>
+            <form>
+              <input type="text" name="orgName" value={name}/>
+
+              Enter an Id: <input type="text" name="value"/>
+              <input type="submit" value="query"/>
+            </form>
+          </div>
         </h4>
       </div>
     );
