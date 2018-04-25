a2cc2007c4a1ca37e8c198641ae7218b294868aa
Nicholas Roth <lou3797@gmail.com>
2018-04-25 08:38:27 -0500

    Import specific function from Organization API

--- packages/app/pages/ModifyOrganization/modifyOrg.tsx
+++ packages/app/pages/ModifyOrganization/modifyOrg.tsx
@@ -12,7 +12,29 @@ import "./modifyOrg.css";
 import getAllOrganizations from "../../../ui-bindings/Organization/getAllOrganizations";
 import { concatArray } from "../Api/common";
 import getOrganizationById from "../../../ui-bindings/Organization/getOrganizationById";
-import Organization from "../Api/organization";
+import GetSingleOrganization from "../Api/organization";
+
+export const Organization = (props) => {
+  let organization = props.organization;
+  return(
+    <div>
+      <div>id: {organization.id}</div>
+      <div>name: {organization.name}</div>
+      <div>type: {organization.type}</div>
+      <div>image: {organization.image}</div>
+      <div>note: {organization.note}</div>
+      <div>ownedEconomicResources: {concatArray(organization.ownedEconomicResources)}</div>
+      <div>agentProcesses: {concatArray(organization.agentProcesses)}</div>
+      <div>agentPlans: {concatArray(organization.agentPlans)}</div>
+      <div>agentEconomicEvents: {concatArray(organization.agentEconomicEvents)}</div>
+      <div>agentCommitments: {concatArray(organization.agentCommitments)}</div>
+      <div>agentRelationships: {concatArray(organization.agentRelationships)}</div>
+      <div>agentRoles: {concatArray(organization.agentRoles)}</div>
+      <div>agentRecipess: {concatArray(organization.agentRecipes)}</div>
+      <br/>
+    </div>
+  );
+};
 
 const OrganizationField = (props) => {
   return(
@@ -39,25 +61,6 @@ export const GetAllOrganizations = getAllOrganizations(({ organizationList, load
     return(
       <div>
         {concatArray(organizationList)}
-      </div>
-    );
-  }
-});
-
-export const GetSingleOrganization = getOrganizationById(({ organization, loading, error }) => {
-
-  if (loading) {
-    return(
-      <strong>Loading...</strong>
-    );
-  } else if (error) {
-    return(
-      <p style={{color: "#F00"}}>API error</p>
-    );
-  } else {
-    return(
-      <div>
-        <Organization organization={organization}/>
       </div>
     );
   }
