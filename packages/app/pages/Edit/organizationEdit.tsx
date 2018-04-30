2e0ce4c5f0de629667ad58902bc4a98b8dabaa11
Nicholas Roth <lou3797@gmail.com>
2018-04-30 04:09:29 -0500

    Added documentation.

--- packages/app/pages/Edit/organizationEdit.tsx
+++ packages/app/pages/Edit/organizationEdit.tsx
@@ -4,7 +4,7 @@
  *
  * @package: REA app
  * @author:  Nicholas Roth <Lou3797>
- * @version: 2018.4.25
+ * @version: 2018.4.30
  * @since:   4/25/2018
  */
 
@@ -13,6 +13,9 @@ import "./organizationEdit.css";
 import getOrganizationById from "../../../ui-bindings/Organization/getOrganizationById";
 import { validatePageLoaded, getValidation } from "../Common/common";
 
+/**
+ * Creates the EditForm for a given Organization.
+ */
 const OrganizationForm = getOrganizationById(({ organization, loading, error }) => {
   let temp = getValidation(loading, error);
   if(temp[0]) {
@@ -24,6 +27,11 @@ const OrganizationForm = getOrganizationById(({ organization, loading, error }) 
   }
 });
 
+/**
+ * A component that encapsulates the entire edit form for an organization. Creates the individual
+ * text fields and submit button. Has local copies of the organization fields that get changed by the
+ * fields via callbacks and then sent to the mutation.
+ */
 class EditForm extends React.Component {
   private state;
 
@@ -38,17 +46,21 @@ class EditForm extends React.Component {
     };
   }
 
-  updateField = (key, val) => {
-    this.setState({[key]: val});
-    console.log("this.props." + key + " is now " + this.state[key]);
+  /**
+   * Updates a given field in the state to the given value
+   * @param i The field being modified
+   * @param v The value to set it to
+   */
+  updateStateField = (i, v) => {
+    this.setState({[i]: v});
+    //console.log("this.props." + i + " is now " + this.state[i]);
   };
 
+  /**
+   * Performs the mutation
+   */
   executeMutation = () => {
-    console.log("Performing mutation");
-    console.log(this.state.name);
-    console.log(this.state.type);
-    console.log(this.state.image);
-    console.log(this.state.note);
+    console.log("Performing mutation\n"+this.state.name+"\n"+this.state.type+"\n"+this.state.image+"\n"+this.state.note);
   };
 
   setEditMode = (bool) => {
@@ -63,13 +75,13 @@ class EditForm extends React.Component {
     return(
       <div>
         <strong>Organization Name:</strong> <br/>
-        <EditTextField text={this.state.name} val={"name"} callback={this.updateField} setEditMode={this.setEditMode}/> <br/>
+        <EditTextField text={this.state.name} val={"name"} callback={this.updateStateField} setEditMode={this.setEditMode}/> <br/>
         <strong>Organization Type:</strong> <br/>
-        <EditTextField text={this.state.type} val={"type"} callback={this.updateField} setEditMode={this.setEditMode}/> <br/>
+        <EditTextField text={this.state.type} val={"type"} callback={this.updateStateField} setEditMode={this.setEditMode}/> <br/>
         <strong>Organization Image:</strong> <br/>
-        <EditTextField text={this.state.image} val={"image"} callback={this.updateField} setEditMode={this.setEditMode}/> <br/>
+        <EditTextField text={this.state.image} val={"image"} callback={this.updateStateField} setEditMode={this.setEditMode}/> <br/>
         <strong>Notes:</strong> <br/>
-        <EditTextField text={this.state.note} val={"note"} callback={this.updateField} setEditMode={this.setEditMode}/> <br/>
+        <EditTextField text={this.state.note} val={"note"} callback={this.updateStateField} setEditMode={this.setEditMode}/> <br/>
         <br/>
         <SubmitInput callback={this.executeMutation} checkMode={this.getEditMode}/>
       </div>
@@ -77,6 +89,10 @@ class EditForm extends React.Component {
   }
 }
 
+/**
+ * A component for a click-to-edit text field. Acts as a regular span element until clicked on, at which point it
+ * instead renders a text input field. The input can be canceled to revert back to the previous data.
+ */
 class EditTextField extends React.Component {
   private state;
 
@@ -88,21 +104,25 @@ class EditTextField extends React.Component {
       safeText: this.props.text
     };
   }
+
   componentDidMount() {
     document.addEventListener('click', this.handleClick);
   }
+
   componentWillUnmount() {
     document.removeEventListener('click', this.handleClick);
   }
+
   handleClick = (e) => {
     if (this.node.contains(e.target)) {
-      //console.log('Clicked in component');
+      //Component was clicked
       this.setEditMode(true);
     } else {
-      //console.log('Clicked out of component');
+      //Component was not clicked
       this.setEditMode(false);
     }
   };
+
   setEditMode(bool) {
     if(!bool) {
       if(this.state.text != this.state.safeText) {
@@ -112,16 +132,27 @@ class EditTextField extends React.Component {
     }
     this.setState({editMode: bool});
   }
+
+  /**
+   * Reverts changes done by resetting the current text to the saved text in the state.
+   */
   revertChanges() {
-    console.log("Changes reverted");
+    //console.log("Changes reverted");
     this.setState({text: this.state.safeText});
     this.props.callback(this.props.val, this.state.text);
     this.setEditMode(false);
   }
+
+  /**
+   * Called whenever the text field is changed. Sets the current text value to the field's value and updates
+   * the respective value of the parent state.
+   * @param e The event
+   */
   updateText = (e) => {
     this.setState({text: e.target.value});
     this.props.callback(this.props.val, e.target.value);
   };
+
   render() {
     let tempText = (this.state.text == "") ? "<empty>" : this.state.text;
     if(this.state.editMode) {
@@ -141,6 +172,9 @@ class EditTextField extends React.Component {
 
 }
 
+/**
+ * The submit button for the edit mutation.
+ */
 class SubmitInput extends React.Component {
   private state;
   constructor(props) {
@@ -148,13 +182,9 @@ class SubmitInput extends React.Component {
     this.state = {};
   }
 
-  createMutation() {
-    this.props.callback();
-  }
-
   render() {
     return (
-      <input type="submit" name="doMutation" value="Submit Changes" onClick={() => this.createMutation()}/>
+      <input type="submit" name="doMutation" value="Submit Changes" onClick={() => this.props.callback}/>
     )
   }
 }
@@ -173,14 +203,3 @@ class App extends React.Component {
 }
 
 export default App;
-
-/*
-<form>
-  <input type="text" name="orgName" defaultValue={organization.name}/> <br/>
-  <input type="text" name="orgType" defaultValue={organization.type}/> <br/>
-  <input type="text" name="orgImage" defaultValue={organization.image}/> <br/>
-  <input type="text" name="orgNote" defaultValue={organization.note}/> <br/>
-  <br/>
-  <input type="submit" value="Submit Changes"/>
-</form>
-*/
