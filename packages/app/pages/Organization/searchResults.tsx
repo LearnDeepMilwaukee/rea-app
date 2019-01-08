0e5b68b3b840c45df7129242b9f5a64bdb2f7279
Donal Moloney <Moloneyda@msoe.edu>
2019-01-07 21:35:14 -0600

    Redid the sort area of the organization page so that the flex box matches the rough outline of the mock up. Still needs more tweaking in the scss file to more precisly match

--- packages/app/pages/Organization/searchResults.tsx
+++ packages/app/pages/Organization/searchResults.tsx
@@ -2,7 +2,7 @@
 import has = Reflect.has;
 import * as React from "react";
 import * as themable from "react-themeable";
-import CardFront from  "./orgCardFront";
+import CardFront from "./orgCardFront";
 import * as cardTheme from "./cardStyle.scss";
 import * as pageTheme from "./searchStyle.scss";
 
@@ -10,10 +10,7 @@ import * as pageTheme from "./searchStyle.scss";
 import getAllOrganizations from "../../../ui-bindings/Organization/getAllOrganizations";
 
 
-
-
 class SearchResults extends React.Component {
-
 
 
   render() {
@@ -22,23 +19,23 @@ class SearchResults extends React.Component {
     let page_theme = themable(pageTheme);
     let search = new URLSearchParams(this.props.location.search);
     let orgname = search.get("searchParams");
-    console.log("Params:"+orgname);
+    console.log("Params:" + orgname);
 
-    const OrgList = getAllOrganizations(({ organizationList, loading, error}) => {
+    const OrgList = getAllOrganizations(({organizationList, loading, error}) => {
 
       if (loading) {
-        return(
-              <strong>Loading...</strong>
-            );
-    } else if (error) {
-        return(<p style={{color: "#F00"}}>API error</p>);
-    } else {
+        return (
+          <strong>Loading...</strong>
+        );
+      } else if (error) {
+        return (<p style={{color: "#F00"}}>API error</p>);
+      } else {
         let filteredOrgs = [];
-        for(let org of organizationList){
-            if (org.name.includes(orgname)){
-              filteredOrgs.push(org);
-              console.log("Matching Org: " + org.name);
-            }
+        for (let org of organizationList) {
+          if (org.name.includes(orgname)) {
+            filteredOrgs.push(org);
+            console.log("Matching Org: " + org.name);
+          }
         }
         const cardsArray = filteredOrgs.map(card => (
           <div>
@@ -47,59 +44,61 @@ class SearchResults extends React.Component {
         ));
 
 //{...card_theme(0,"card")}
-        return(
+        return (
           <div>
-            <div>
+            <div {...page_theme(0,"search-container")}>
               <h2>All orgs</h2>
-              <span>
-                   <div>
-                     <input type={'text'} name={'SearchBox'}  placeholder={'Search'}/><br/>
-                     <span>
+              <div><form><input type="text" name="name"/></form>
                        <input type={'checkbox'} name={'myOrgsFilter'}/>
                        <label>My Orgs</label>
                        <input type={'checkbox'} name={'publicOrgsFilter'}/>
                        <label>Public</label>
                        <input type={'checkbox'} name={'placeholderFilter'}/>
-                       <label>Placeholder</label>
-                      </span>
-                   </div>
-                   <div>
-                     <span>
-                        <label>Max Distance:</label>
-                        <input type={'text'} name={'maxDistText'}/>
-                     </span>
-                     <span>
-                       <label>0 mi</label>
-                        <input type={'range'} name={'maxDistRange'} value={30}/>
-                       <label>100 mi</label>
-                     </span>
-                   </div>
-                   <div>
-                     <label>Type:</label>
-                     <select>
-                       <option value={'school'}>School</option>
-                     </select>
-                   </div>
-                   <div>
-                     <label>Sort:</label>
-                     <select>
-                       <option value={'alphabetical'}>Alphabetical</option>
-                       <option value={'distance'}>Distance</option>
-                     </select>
-                   </div>
-              </span>
-            </div>
+              </div>
+
+              <div>
+                <div>
+                  <label>Max Distance:</label>
+                  <input type={'text'} name={'maxDistText'}/>
+                </div>
+                <div>
+                  <input type={'range'} name={'maxDistRange'}/>
+                </div>
+
+              </div>
+                <div>
+                  <label>Type:</label>
+                  <select>
+                    <option value={'school'}>School</option>
+                    <option value={'church'}>Church</option>
+                    <option value={'for-profit company'}>For-profit Company</option>
+                    <option value={'individual'}>Individual</option>
+                    <option value={'organization'}>Organization</option>
+                    <option value={'library'}>Library</option>
+                    <option value={'makerspace'}>Makerspace</option>
+                    <option value={'network'}>Network</option>
+                    <option value={'non-profit'}>Non-profit</option>
+                  </select>
+                </div>
+                <div>
+                  <label>Sort:</label>
+                  <select>
+                    <option value={'alphabetical'}>Alphabetical</option>
+                    <option value={'distance'}>Distance</option>
+                  </select>
+                </div>
+              </div>
             <div>
-            {cardsArray}
+              {cardsArray}
             </div>
           </div>
         );
-    }
+      }
     });
 
-    return(
-      <div >
-          <OrgList cardData={} />
+    return (
+      <div>
+        <OrgList cardData={}/>
       </div>
     )
   }
