6219cac0f86464dde301b4e34d547a50f0e7f97b
Michael Larson <larsonme@msoe.edu>
2019-01-12 16:31:05 -0600

    Updated the sorting and filtering elements code so that work and render correctly.

--- packages/app/pages/Organization/organizationCatalog.tsx
+++ packages/app/pages/Organization/organizationCatalog.tsx
@@ -14,12 +14,14 @@ class SearchResults extends React.Component {
 
   constructor() {
     super();
-    this.handleReset = this.handleReset.bind(this);
     this.state = {
       sorting: "alphabetical",
       typeFilter: "All",
       distanceFilter: 50,
-      load: true
+      load: true,
+      nameFilter: "",
+      myOrgsFilter:false,
+      pubOrgsFilter: true
     };
   }
 
@@ -37,21 +39,32 @@ class SearchResults extends React.Component {
     this.setState({distanceFilter:value});
   };
 
-  handleReset (event) {
+  handleReset = function(value) {
     console.log("CLCIKEDCKED");
-    this.setState({distanceFilter:"50"});
-    this.setState({typeFilter:"alphabetical"});
-    this.setState({sorting:"All"});
+    // Update state here
+    // this.setState({distanceFilter: 50,sorting:"alphabetical",typeFilter:"All", nameFilter: "", myOrgsFilter: false, pubOrgsFilter:true});
 
-  }
+  };
+  onNameChange = function (value) {
+    console.log(value);
+    this.setState({nameFilter:value});
+  };
+  onMyOrgsChange = function (value) {
+    console.log("myOrgs: " + value.toString());
+    this.setState({myOrgsFilter:value});
+  };
+  onPubOrgsChange = function (value) {
+    console.log("pubOrgs: " + value.toString());
+    this.setState({pubOrgsFilter:value});
+  };
+
+
 
   render() {
-
-    let card_theme = themable(cardTheme);
+    console.log("Start");
+    console.log(this.state);
     let page_theme = themable(pageTheme);
-    let search = new URLSearchParams(this.props.location.search);
     let msoeCC = {latitude:43.044004,longitude:-87.909020};
-    let orgname = search.get("nameSearchParam");
     const OrgList = getAllOrganizations(({organizationList, loading, error}) => {
 
       if (loading) {
@@ -64,9 +77,9 @@ class SearchResults extends React.Component {
         let filteredOrgs = [];
 
         for(let org of organizationList){
-          if(isNullOrUndefined(orgname) || org.name.includes(orgname)){
+          if(isNullOrUndefined(this.state.nameFilter) || org.name.includes(this.state.nameFilter)){
             filteredOrgs.push(org);
-            console.log("Matching Org: " + org.name);
+            // console.log("Matching Org: " + org.name);
           }
         }
           filteredOrgs = filterByDistance(filteredOrgs,this.state.distanceFilter,msoeCC);
@@ -96,26 +109,18 @@ class SearchResults extends React.Component {
             <h2 {...page_theme(8,"org-category")}>All orgs</h2>
             <div {...page_theme(4,"search-container")}>
               <div {...page_theme(5,"search-filter-group")}>
-                <form>
-                       <input type="text" name="nameSearchParam" defaultValue={orgname}/>
-                       <input type={'checkbox'} name={'myOrgsFilter'} checked={search.get('myOrgsFilter') === 'on'} onClick={this.checked = !this.checked}/>
-                       <label>My Orgs</label>
-                  {/*If there is no nameSearchParam, then this button should be checked since it is the initial load*/}
-                       <input type={'checkbox'} name={'publicOrgsFilter'} checked={search.get('publicOrgsFilter') === 'on' || !search.has("nameSearchParam")} onClick={this.checked = !this.checked}/>
-                       <label>Public</label>
-                </form>
-                </div>
 
+                <input autoFocus type="text" onChange={(event) => this.onNameChange(event.target.value)} defaultValue={this.state.nameFilter}/>
+                <label>My Orgs</label>
+                <input type="checkbox" checked={this.state.myOrgsFilter} onClick={(event) => {this.checked = !this.checked; this.onMyOrgsChange(event.target.checked)}}/>
+                <label>Public</label>
+                <input type="checkbox" checked={this.state.pubOrgsFilter} onClick={(event) => {this.checked = !this.checked; this.onPubOrgsChange(event.target.checked)}}/>
                 <div {...page_theme(6,"search-dist-group")}>
-                <div>
                   <label {...page_theme(9,"max-distance-label")}>Max Distance(mi):</label>
                   <input onChange={(event) => this.onDistanceFilterChange(event.target.value)} type={'text'} name={'maxDistText'} value={50} style={{width:'40px'}}/>
-                </div>
-                <div>
-                  <input type={'range'} name={'maxDistRange'}/>
-                </div>
-
-                </div >
+                  <div>
+                    <input type={'range'} name={'maxDistRange'}/>
+                  </div>
                 <div{...page_theme(7,"search-filter-group")}>
                   <label>Type:</label><br/>
                   <select onChange={(event) => this.onTypeFilterChange(event.target.value)} style={{width:'100px'}}>
@@ -133,13 +138,20 @@ class SearchResults extends React.Component {
                 </div>
                 <div {...page_theme(8,"search-filter-group")}>
                   <label>Sort:</label>
-                     <select onChange={(event) => this.onSortFilterChange(event.target.value)}>
-                       <option selected = {this.state.sorting === 'alphabetical'} value={'alphabetical'}>Alphabetical</option>
-                       <option selected = {this.state.sorting === 'distance'} value={'distance'}>Distance</option>
-                     </select>
+                  <select onChange={(event) => this.onSortFilterChange(event.target.value)}>
+                    <option selected = {this.state.sorting === 'alphabetical'} value={'alphabetical'}>Alphabetical</option>
+                    <option selected = {this.state.sorting === 'distance'} value={'distance'}>Distance</option>
+                  </select>
                 </div>
+
+                </div>
+
+
+
+                </div >
+
                 <div {...page_theme(12,"search-filter-group")}>
-                  <button {...page_theme(13,"negative-button")}>reset filters</button>
+                  <button {...page_theme(13,"negative-button")} onClick={this.handleReset}>reset filters</button>
                 </div>
               </div>
             <div>
