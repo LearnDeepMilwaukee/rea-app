7fa6f2ac00a90a2b87775b7ade4390bd5b1961c7
Michael Larson <larsonme@msoe.edu>
2019-01-14 09:51:26 -0600

    Removed unneeded files and imports.  Also updated the cards to only show the distance to that organization to one decimal place.

--- packages/app/pages/Organization/orgCard.tsx
+++ packages/app/pages/Organization/orgCard.tsx
@@ -1,144 +1,38 @@
-import * as React from "react";
-import ReactDOM from "react-dom";
-// const cardContainer = document.querySelector('.react-card');
-
-// React component for form inputs
-class CardInput extends React.Component {
-  render() {
-    return(
-      <fieldset>
-        <input name={this.props.name} id={this.props.id} type={this.props.type || 'text'} placeholder={this.props.placeholder} required />
-      </fieldset>
-    )
-  }
-}
-
-// React component for textarea
-class CardTextarea extends React.Component {
-  render() {
-    return(
-      <fieldset>
-        <textarea name={this.props.name} id={this.props.id} placeholder={this.props.placeholder} required ></textarea>
-      </fieldset>
-    )
-  }
-}
-
-// React component for form button
-class CardBtn extends React.Component {
-  render() {
-    return(
-      <fieldset>
-        <button className={this.props.className} type={this.props.type} value={this.props.value}>{this.props.value}</button>
-      </fieldset>
-    )
-  }
-}
-
-// React component for social profile links
-class CardProfileLinks extends React.Component {
-  render() {
-    const profileLinks = ['twitter', 'linkedin', 'dribbble', 'facebook'];
-
-    const linksList = profileLinks.map((link, index) =>
-      <li key={index}><a href='#'><i className={'fa fa-' + link}></i></a></li>
-    );
-
-    return(
-      <div className='card-social-links'>
-        <ul className='social-links'>
-          {linksList}
-        </ul>
-      </div>
-    )
-  }
-}
-
 // React component for the frontside of the card
+import * as React from "react";
+import * as themable from "react-themeable";
+import * as cardTheme from "./cardStyle.scss";
+import {isNullOrUndefined} from "util";
+
+
 class CardFront extends React.Component {
+
+
   render() {
+    let org = this.props.card;
+    let name = org.name;
+    let distance = this.props.distance;
+    let type = org.type;
+    let note = org.note;
+    let org_image = org.image;
+    let card_theme = themable(cardTheme);
+    let alt_desc = require("./defaultuserimg.jpg"); // Way of using images in react it has to be done this way for web pack to recognize it. Doing reacts way does not work, only the webpack specific way works.
+    console.log("DEFAULT IMG    "+ alt_desc);
+    if(org_image.trim() == "" || org_image.trim().length == 0){ // If no image is returned the default user profile picture is shown. This also occurs if there is an issue displaying the image it displays the default user image
+      org_image = alt_desc; // Sets the image to the alternative image
+    }
     return(
-      <div className='card-side side-front'>
-        <div className='container-fluid'>
-          <div className='row'>
-            <div className='col-xs-6'>
-              <img src='https://source.unsplash.com/w8YICpz1I10/358x458' />
-            </div>
+      <span {...card_theme(0,"card")}>
 
-            <div className='col-xs-6 side-front-content'>
-              <h2>Czech based</h2>
-
-              <h1>UI/UX Designer</h1>
-
-              <p>Andrey is driven by turning ideas into scalable and and empowering experiences that solve real life problems.</p>
-
-              <p>He is currently the founder of Dvorak Media. Previously, Andrey was a product designer at Dropbox.</p>
-
-              <p>Over the years, Michael has been priviledged to have worked with Adobe, Evernote, Square and more.</p>
-            </div>
+        <img src={isNullOrUndefined(org_image) ?  alt_desc : org_image} {...card_theme(4,"img")}/>
+        <h2 id="name" {...card_theme(1,"name")}>{name}</h2>
+        <div {...card_theme(2,"details")}>
+          <h3 id="type">{"Type: " + type}</h3>
+          <h3 id="note">{isNullOrUndefined(distance) ? "Distance: not available" : "Distance: " + distance + " mi."}</h3>
           </div>
-        </div>
-      </div>
-    )
+      </span>
+  )
   }
 }
 
-// React component for the backside of the card
-class CardBack extends React.Component {
-  render() {
-    return(
-      <div className='card-side side-back'>
-        <div className='container-fluid'>
-          <h1>Let's get in touch!</h1>
-
-          <form formAction='' className='card-form'>
-            <div className='row'>
-              <div className='col-xs-6'>
-                <CardInput name='contactFirstName' id='contactFirstName' type='text' placeholder='Your first name' />
-              </div>
-
-              <div className='col-xs-6'>
-                <CardInput name='contactLastName' id='contactLastName' type='text' placeholder='Your last name' />
-              </div>
-            </div>
-
-            <div className='row'>
-              <div className='col-xs-6'>
-                <CardInput name='contactEmail' id='contactEmail' type='email' placeholder='Your email address' />
-              </div>
-
-              <div className='col-xs-6'>
-                <CardInput name='contactSubject' id='contactSubject' type='text' placeholder='Subject' />
-              </div>
-            </div>
-
-            <CardTextarea name='contactMessage' id='contactMessage' placeholder='Your message' />
-
-            <CardBtn className='btn btn-primary' type='submit' value='Send message' />
-          </form>
-
-          <CardProfileLinks />
-        </div>
-      </div>
-    )
-  }
-}
-
-// React component for the card (main component)
-class Card extends React.Component {
-  render() {
-    return(
-      <div className='card-container'>
-        <div className='card-body'>
-          <CardBack />
-
-          <CardFront />
-        </div>
-      </div>
-    )
-  }
-}
-
-// Render Card component
-// ReactDOM.render(<Card />);
-export default Card();
+export default CardFront;
--- packages/app/pages/Organization/organizationCatalog.tsx
+++ packages/app/pages/Organization/organizationCatalog.tsx
@@ -1,9 +1,7 @@
 // React component for the frontside of the card
-import has = Reflect.has;
 import * as React from "react";
 import * as themable from "react-themeable";
-import CardFront from "./orgCardFront";
-import * as cardTheme from "./cardStyle.scss";
+import CardFront from "./orgCard";
 import * as pageTheme from "./catalogStyle.scss";
 import {sortByName, sortByDistance, getDistanceBetweenPoints, filterByType, filterByDistance} from "./utilities"
 import getAllOrganizations from "../../../ui-bindings/Organization/getAllOrganizations";
@@ -20,39 +18,46 @@ class SearchResults extends React.Component {
       distanceFilter: 50,
       load: true,
       nameFilter: "",
-      myOrgsFilter:false,
+      myOrgsFilter: false,
       pubOrgsFilter: true
     };
   }
 
-  onSortFilterChange = function (value){
-    this.setState({sorting:value});
+  onSortFilterChange = function (value) {
+    this.setState({sorting: value});
   };
 
-  onTypeFilterChange = function(value) {
-    this.setState({typeFilter:value});
+  onTypeFilterChange = function (value) {
+    this.setState({typeFilter: value});
   };
 
-  onDistanceFilterChange = function(event) {
-    if(event.key === 'Enter'){
+  onDistanceFilterChange = function (event) {
+    if (event.key === 'Enter') {
       this.setState({distanceFilter: event.target.value});
     }
   };
 
-  handleReset = function(env, value) {
-    env.setState({distanceFilter: 50,sorting:"alphabetical",typeFilter:"All", nameFilter: "", myOrgsFilter: false, pubOrgsFilter:true});
+  handleReset = function (env, value) {
+    env.setState({
+      distanceFilter: 50,
+      sorting: "alphabetical",
+      typeFilter: "All",
+      nameFilter: "",
+      myOrgsFilter: false,
+      pubOrgsFilter: true
+    });
 
   };
   onNameChange = function (event) {
-    if(event.key === 'Enter') {
+    if (event.key === 'Enter') {
       this.setState({nameFilter: event.target.value});
     }
   };
   onMyOrgsChange = function (value) {
-    this.setState({myOrgsFilter:value});
+    this.setState({myOrgsFilter: value});
   };
   onPubOrgsChange = function (value) {
-    this.setState({pubOrgsFilter:value});
+    this.setState({pubOrgsFilter: value});
   };
 
   notImplementedFunction = function () {
@@ -60,10 +65,9 @@ class SearchResults extends React.Component {
   };
 
 
-
   render() {
     let page_theme = themable(pageTheme);
-    let msoeCC = {latitude:43.044004,longitude:-87.909020};
+    let msoeCC = {latitude: 43.044004, longitude: -87.909020};
     const OrgList = getAllOrganizations(({organizationList, loading, error}) => {
 
       if (loading) {
@@ -75,88 +79,109 @@ class SearchResults extends React.Component {
       } else {
         let filteredOrgs = [];
 
-        for(let org of organizationList){
-          if(isNullOrUndefined(this.state.nameFilter) || org.name.includes(this.state.nameFilter)){
+        for (let org of organizationList) {
+          if (isNullOrUndefined(this.state.nameFilter) || org.name.includes(this.state.nameFilter)) {
             filteredOrgs.push(org);
             console.log("Org matching filter " + this.state.nameFilter + ": " + org.name);
           }
         }
-          if(this.state.distanceFilter!='') {
-            filteredOrgs = filterByDistance(filteredOrgs, this.state.distanceFilter, msoeCC);
-          }
-          if(this.state.typeFilter !== "All") {
-            filteredOrgs = filterByType(filteredOrgs, this.state.typeFilter);
-          }
-          if(this.state.sorting === "alphabetical"){
-            filteredOrgs = sortByName(filteredOrgs);
-          }
-          else if(this.state.sorting === "distance"){
-            filteredOrgs = sortByDistance(filteredOrgs,msoeCC);
-          }
+        if (this.state.distanceFilter != '') {
+          filteredOrgs = filterByDistance(filteredOrgs, this.state.distanceFilter, msoeCC);
+        }
+        if (this.state.typeFilter !== "All") {
+          filteredOrgs = filterByType(filteredOrgs, this.state.typeFilter);
+        }
+        if (this.state.sorting === "alphabetical") {
+          filteredOrgs = sortByName(filteredOrgs);
+        }
+        else if (this.state.sorting === "distance") {
+          filteredOrgs = sortByDistance(filteredOrgs, msoeCC);
+        }
 
         const cardsArray = filteredOrgs.map(card => (
-          <div><span {...page_theme(4,"org-card-button")}>
-            <CardFront card={card} distance={isNullOrUndefined(card.primaryLocation) ? null : getDistanceBetweenPoints(card.primaryLocation,msoeCC).toFixed(3)}/>
-            <button {...page_theme(3,"primary-button")} onClick={this.notImplementedFunction}>connect</button>
+          <div><span {...page_theme(4, "org-card-button")}>
+            <CardFront card={card}
+                       distance={isNullOrUndefined(card.primaryLocation) ? null : getDistanceBetweenPoints(card.primaryLocation, msoeCC).toFixed(1)}/>
+            <button {...page_theme(3, "primary-button")} onClick={this.notImplementedFunction}>connect</button>
           </span>
           </div>
         ));
-        if(cardsArray.length == 0){
-          cardsArray = <p {...page_theme(10,"no-results")}> No search results</p>;
+        if (cardsArray.length == 0) {
+          cardsArray = <p {...page_theme(10, "no-results")}> No search results</p>;
         }
 
         return (
-          <div {...page_theme(1,"page")}>
-            <h2 {...page_theme(8,"org-category")}>All orgs</h2>
-            <div {...page_theme(4,"search-container")}>
-              <div {...page_theme(5,"search-filter-group")}>
+          <div {...page_theme(1, "page")}>
+            <h2 {...page_theme(8, "org-category")}>All orgs</h2>
+            <div {...page_theme(4, "search-container")}>
+              <div {...page_theme(5, "search-filter-group")}>
                 <div>
-                  <input name={'orgName'} type="text" onKeyPress={(event) => this.onNameChange(event)} defaultValue={this.state.nameFilter}/>
+                  <input name={'orgName'} type="text" onKeyPress={(event) => this.onNameChange(event)}
+                         defaultValue={this.state.nameFilter}/>
                 </div>
-                <input type="checkbox" checked={this.state.myOrgsFilter} onClick={(event) => {this.checked = !this.checked; this.onMyOrgsChange(event.target.checked)}}/>
+                <input type="checkbox" checked={this.state.myOrgsFilter} onClick={(event) => {
+                  this.checked = !this.checked;
+                  this.onMyOrgsChange(event.target.checked)
+                }}/>
                 <label>My Orgs</label>
 
-                <input type="checkbox" checked={this.state.pubOrgsFilter} onClick={(event) => {this.checked = !this.checked; this.onPubOrgsChange(event.target.checked)}}/>
+                <input type="checkbox" checked={this.state.pubOrgsFilter} onClick={(event) => {
+                  this.checked = !this.checked;
+                  this.onPubOrgsChange(event.target.checked)
+                }}/>
                 <label>Public</label>
 
               </div>
-                <div {...page_theme(6,"search-dist-group")}>
-                  <label {...page_theme(9,"max-distance-label")}>Max Distance(mi):</label>
-                  <input onKeyPress={(event) => {this.onDistanceFilterChange(event)}} type={'number'} min={1} name={'maxDistText'} defaultValue={this.state.distanceFilter} style={{width:'50px'}}/>
-                  <div><span>
+              <div {...page_theme(6, "search-dist-group")}>
+                <label {...page_theme(9, "max-distance-label")}>Max Distance(mi):</label>
+                <input onKeyPress={(event) => {
+                  this.onDistanceFilterChange(event)
+                }} type={'number'} min={1} name={'maxDistText'} defaultValue={this.state.distanceFilter}
+                       style={{width: '50px'}}/>
+                <div><span>
                     <label>1 mi</label>
-                    <input type={'range'} name={'maxDistRange'} min={1} max={100} onMouseUp={(event) => {this.onDistanceFilterChange(event.target.value)}} defaultValue={this.state.distanceFilter}/>
+                    <input type={'range'} name={'maxDistRange'} min={1} max={100} onMouseUp={(event) => {
+                      this.onDistanceFilterChange(event.target.value)
+                    }} defaultValue={this.state.distanceFilter}/>
                   <label>100 mi</label></span>
-                  </div>
-                </div>
-                <div{...page_theme(7,"search-filter-group")}>
-                  <label>Type:</label><br/>
-                  <select onChange={(event) => this.onTypeFilterChange(event.target.value)} style={{width:'100px'}}>
-                    <option selected = {this.state.typeFilter === 'All'} value={'All'}>All</option>
-                    <option selected = {this.state.typeFilter === 'School'} value={'School'}>School</option>
-                    <option selected = {this.state.typeFilter === 'Church'} value={'Church'}>Church</option>
-                    <option selected = {this.state.typeFilter === 'For-profit Company'} value={'For-Profit Company'}>For-profit Company</option>
-                    <option selected = {this.state.typeFilter === 'dividual'} value={'Individual'}>Individual</option>
-                    <option selected = {this.state.typeFilter === 'Organization'} value={'Organization'}>Organization</option>
-                    <option selected = {this.state.typeFilter === 'Library'} value={'Library'}>Library</option>
-                    <option selected = {this.state.typeFilter === 'Makerspace'} value={'Makerspace'}>Makerspace</option>
-                    <option selected = {this.state.typeFilter === 'Network'} value={'Network'}>Network</option>
-                    <option selected = {this.state.typeFilter === 'Non-profit Company'} value={'Non-profit Company'}>Non-profit Company</option>
-                  </select>
-                </div>
-                <div {...page_theme(8,"search-filter-group")}>
-                  <label>Sort:</label>
-                  <select onChange={(event) => this.onSortFilterChange(event.target.value)}>
-                    <option selected = {this.state.sorting === 'alphabetical'} value={'alphabetical'}>Alphabetical</option>
-                    <option selected = {this.state.sorting === 'distance'} value={'distance'}>Distance</option>
-                  </select>
-                </div>
-
-
-                <div {...page_theme(12,"search-filter-group")}>
-                  <button {...page_theme(13,"negative-button")} onClick={(event) => {this.handleReset(this,event.target.value)}}>Reset filters</button>
                 </div>
               </div>
+              <div{...page_theme(7, "search-filter-group")}>
+                <label>Type:</label><br/>
+                <select onChange={(event) => this.onTypeFilterChange(event.target.value)} style={{width: '100px'}}>
+                  <option selected={this.state.typeFilter === 'All'} value={'All'}>All</option>
+                  <option selected={this.state.typeFilter === 'School'} value={'School'}>School</option>
+                  <option selected={this.state.typeFilter === 'Church'} value={'Church'}>Church</option>
+                  <option selected={this.state.typeFilter === 'For-profit Company'} value={'For-Profit Company'}>
+                    For-profit Company
+                  </option>
+                  <option selected={this.state.typeFilter === 'dividual'} value={'Individual'}>Individual</option>
+                  <option selected={this.state.typeFilter === 'Organization'} value={'Organization'}>Organization
+                  </option>
+                  <option selected={this.state.typeFilter === 'Library'} value={'Library'}>Library</option>
+                  <option selected={this.state.typeFilter === 'Makerspace'} value={'Makerspace'}>Makerspace</option>
+                  <option selected={this.state.typeFilter === 'Network'} value={'Network'}>Network</option>
+                  <option selected={this.state.typeFilter === 'Non-profit Company'} value={'Non-profit Company'}>
+                    Non-profit Company
+                  </option>
+                </select>
+              </div>
+              <div {...page_theme(8, "search-filter-group")}>
+                <label>Sort:</label>
+                <select onChange={(event) => this.onSortFilterChange(event.target.value)}>
+                  <option selected={this.state.sorting === 'alphabetical'} value={'alphabetical'}>Alphabetical</option>
+                  <option selected={this.state.sorting === 'distance'} value={'distance'}>Distance</option>
+                </select>
+              </div>
+
+
+              <div {...page_theme(12, "search-filter-group")}>
+                <button {...page_theme(13, "negative-button")} onClick={(event) => {
+                  this.handleReset(this, event.target.value)
+                }}>Reset filters
+                </button>
+              </div>
+            </div>
             <div>
               {cardsArray}
             </div>
