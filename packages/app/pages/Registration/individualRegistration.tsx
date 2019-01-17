7da49a929a2d9460c647d9f306b048df8e6e53b9
Aaron Murphy <murphyad@msoe.edu>
2019-01-17 13:28:22 -0600

    Tested code and began cleaning up for pull request

--- packages/app/pages/Registration/individualRegistration.tsx
+++ packages/app/pages/Registration/individualRegistration.tsx
@@ -4,14 +4,14 @@
  */
 
 import * as React from "react";
-import createUserPerson from "../../../ui-bindings/user/CreateUserPerson.tsx";
-import userEmailExist from "../../../ui-bindings/user/UserEmailExists.tsx";
+import createUserPerson from "../../../ui-bindings/user/CreateUserPerson";
+import userEmailExist from "../../../ui-bindings/user/UserEmailExists";
 import * as EmailValidator from "email-validator";
-import {adminToken} from "../../../../apiKeys.json"
+import { adminToken } from "../../../../apiKeys.json";
 import * as themeable from 'react-themeable';
-import * as theme from "./individualRegistration.scss"
+import * as theme from "./individualRegistration.scss";
 
-let userInformation ={
+let userInformation = {
      email: "",
      pswd: "",
      username: "",
@@ -27,23 +27,20 @@ const EmailFieldConst = (props) => {
     <div>
         <span>
           <text>Email</text>
-          <text {...currentTheme(3,"required")}>*</text><input {...currentTheme(4,"emailBox")} type="text" name="email" onChange={(event) => props.saveEmail(event.target.value)}/>
+          <text {...currentTheme(3, "required")}>*</text><input {...currentTheme(4, "emailBox")} type="text" name="email" onChange={(event) => props.saveEmail(event.target.value)}/>
         </span>
     </div>
   );
 };
-
 
 class UsernameField extends React.Component {
   public state = {
     value: ""
   };
 
-
   onChange = (value) => {
     this.setState({value: value});
     this.props.saveUsername(value);
-
   };
 
   render() {
@@ -51,7 +48,7 @@ class UsernameField extends React.Component {
     return (
       <div>
         <text>Username</text>
-        <text {...currentTheme(0,"required")}>*</text><input {...currentTheme(5,"usernameBox")} type="text" name="username" onChange={(event) => this.onChange(event.target.value)}/>
+        <text {...currentTheme(0, "required")}>*</text><input {...currentTheme(5, "usernameBox")} type="text" name="username" onChange={(event) => this.onChange(event.target.value)}/>
 
       </div>
     );
@@ -63,23 +60,23 @@ class PasswordField extends React.Component {
     passwordOne: undefined,
     passwordTwo: undefined
   };
+
   updateFirstPassword = (value) => {
     this.setState({passwordOne: value}, () => {
       if (this.state.passwordOne === this.state.passwordTwo) {
         this.props.savePassword(value);
-      }
-      else {
+      } else {
         this.props.savePassword("");
       }
     });
 
   };
+
   updateSecondPassword = (value) => {
     this.setState({passwordTwo: value}, () => {
       if (this.state.passwordOne === this.state.passwordTwo) {
         this.props.savePassword(value);
-      }
-      else {
+      } else {
         this.props.savePassword("");
       }
     });
@@ -90,13 +87,12 @@ class PasswordField extends React.Component {
     return (
       <div>
         <text>Password</text>
-        <text {...currentTheme(1,"required")}>*</text><input {...currentTheme(6,"passwordBox")} type="password" name="password" onChange={(event) => this.updateFirstPassword(event.target.value)} />
+        <text {...currentTheme(1, "required")}>*</text><input {...currentTheme(6, "passwordBox")} type="password" name="password" onChange={(event) => this.updateFirstPassword(event.target.value)} />
 
         <br/>
         <br/>
         <text>Confirm Password</text>
-        <text {...currentTheme(2,"required")}>*</text><input {...currentTheme(7,"confirmPasswordBox")} type="password" name="password"
-               onChange={(event) => this.updateSecondPassword(event.target.value)}/>
+        <text {...currentTheme(2, "required")}>*</text><input {...currentTheme(7, "confirmPasswordBox")} type="password" name="password" onChange={(event) => this.updateSecondPassword(event.target.value)}/>
 
         <br/>
       </div>
@@ -107,25 +103,21 @@ class PasswordField extends React.Component {
 const EmailExistsQuery = userEmailExist(({emailExists, loading, error}) => {
   if (loading) {
     console.log("Loading " + loading);
-    return     <p> </p>;
+    return     <p/>;
   } else if (error) {
     console.log("Error: " + error);
     return <h3>Error!</h3>;
   }
-
-    if (emailExists === false) {
-      return <CreateUser userEntered={userInformation.userRan} allValid={userInformation.allValid}
-                         username={userInformation.username} email={userInformation.email}
-                         pswd={userInformation.pswd} name={userInformation.name} image={userInformation.image}
-                         />
-    }
-    else if(emailExists === true){
+  if (emailExists === false) {
+    return <CreateUser userEntered={userInformation.userRan} allValid={userInformation.allValid}
+                       username={userInformation.username} email={userInformation.email}
+                       pswd={userInformation.pswd} name={userInformation.name} image={userInformation.image}
+            />
+    } else if(emailExists === true) {
       alert("An account with this email already exists");
-      return <p> </p>
+      return <p/>;
     }
-
 });
-
 
 const CreateUserQuery = createUserPerson(({createUserPersonVar, error}) => {
 
@@ -133,31 +125,31 @@ const CreateUserQuery = createUserPerson(({createUserPersonVar, error}) => {
     if (error.message.indexOf("username") !== -1) {
       alert("This username is already in use");
     }
-  }
-  else if (createUserPersonVar != null) {
+  } else if (createUserPersonVar != null) {
     return <p>{createUserPersonVar.split(',')[0]}</p>
   } else {
     return <p>{createUserPersonVar}</p>;
   }
 });
 
-
 function CreateUser(props) {
 
   let element;
 
   if (props.userEntered && props.allValid) {
-      element = <CreateUserQuery username={props.username} email={props.email} pswd={props.pswd}
-                                 name={props.name}
-                                 token={adminToken}
-                                 image={props.image}/>
-
+      element = (
+        <CreateUserQuery
+          username={props.username}
+          email={props.email}
+          pswd={props.pswd}
+          name={props.name}
+          token={adminToken}
+          image={props.image}
+        />
+      );
   }
-  return (
-    <div>{element}</div>
-  );
+  return <div>{element}</div>;
 }
-
 
 class IndividualRegistration extends React.Component {
   private state = {
@@ -165,13 +157,11 @@ class IndividualRegistration extends React.Component {
     validEmail: false,
   };
 
-
   handleClick = (event) => {
     event.preventDefault();
 
-
     let message = "";
-    if(!EmailValidator.validate(userInformation.email)){
+    if (!EmailValidator.validate(userInformation.email)) {
       message += "Invalid email\n";
     }
     if (userInformation.pswd === "") {
@@ -181,16 +171,14 @@ class IndividualRegistration extends React.Component {
       message += "Please enter a username\n";
     }
 
-    if(message !== ""){
+    if (message !== ""){
       alert(message);
       return;
-    }
-    else {
+    } else {
       this.setState({queryingEmail: userInformation.email});
       userInformation.userRan = true;
       userInformation.allValid = true;
     }
-
   };
 
   saveEmail = (email) => {
@@ -206,6 +194,7 @@ class IndividualRegistration extends React.Component {
   runEmailExists = () => {
     return <EmailExistsQuery email={this.state.queryingEmail} token={adminToken} allValid={userInformation.allValid}/>
 };
+
   render() {
     return (
       <div>
