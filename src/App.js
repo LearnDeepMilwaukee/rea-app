1b399faab0ce2f40680980c7e55e864e67f98fa9
Michael Larson <larsonme@msoe.edu>
2019-03-06 21:06:32 -0600

    Started working with React Semantic UI some more, really like it so far.  Got a rough setup of the orgs listing, still have to implement the sorting/filtering but that is just moving alot of code from our current project over(stuff we wrote that works well).  Will talk to the group about possibly joining together and working on another page also to see if we want to get that going.

--- src/App.js
+++ src/App.js
@@ -1,64 +1,64 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { ApolloProvider } from "react-apollo";
+import React, {Component} from 'react';
+import {ApolloProvider} from "react-apollo";
 import ApolloClient from "apollo-boost";
-import getAllAgents from "./getAllAgents.js";
+import getAllOrganizations from "./getAllOrganizations.js";
+import {Item} from 'semantic-ui-react'
+import 'semantic-ui-css/semantic.min.css'
+import {isNullOrUndefined} from "util";
 
 const client = new ApolloClient({uri: "http://localhost:8000/api/graph"});
+let default_image = require("./defaultuserimg.jpg");
+const orgCard = (agent) => (
+    <Item>
+        <Item.Image src={isNullOrUndefined(agent.image) || agent.image === "" ? default_image : agent.image}/>
 
-const GetAllAgents = getAllAgents(({ agent, loading, error}) => {
-    console.log("Running");
+        <Item.Content>
+            <Item.Header as='p'>{agent.name}</Item.Header>
+            <Item.Meta>
+                <span>{isNullOrUndefined(agent.primaryLocation) ? "" : agent.primaryLocation.address}</span>
+            </Item.Meta>
+            <Item.Description>{agent.note}</Item.Description>
 
+        </Item.Content>
+    </Item>
+);
+
+const OrgList = getAllOrganizations(({organizationList, loading, error}) => {
     if (loading) {
         console.log("LOADING");
-
-        return(
+        return (
             <strong>Loading...</strong>
         );
     } else if (error) {
-        console.log("ERROR");
+        console.log(error);
 
-        return(
+        return (
             <p style={{color: "#F00"}}>API error</p>
         );
     } else {
-        console.log("SUCCESS");
-        console.log(agent);
-
-        return(
+        console.log(organizationList);
+        const cardsArray = organizationList.map(org => (
+            orgCard(org)
+        ));
+        return (
             <div>
-                <p>Check for agent list in console</p>
+                <Item.Group divided>
+                    {cardsArray}
+                </Item.Group>
             </div>
         );
     }
 });
 
-
 class App extends Component {
-  render() {
-    return (
-        <ApolloProvider client={client}>
-        <GetAllAgents/>
-        <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-        </ApolloProvider>
-    );
-  }
+    render() {
+        return (
+            <ApolloProvider client={client}>
+                <OrgList
+                    token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImNvbm5vciIsImlhdCI6MTU1MTg0ODI3MSwicGFzc3dvcmQiOiI3YzA4ODliOWU5ZmNjYzAxZDIzMDcwNzljNDk5OTcyNDFlNTZlNzU0IiwiaWQiOjZ9.unIuk6g8HcmyIuF1sONrLAiftApTlcuqMWWLO6DtqUQ"/>
+            </ApolloProvider>
+        );
+    }
 }
 
 export default App;
