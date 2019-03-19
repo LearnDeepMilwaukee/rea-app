7807e5069e78866320be7625469d303608ba2a46
Michael Larson <larsonme@msoe.edu>
2019-03-19 15:22:38 -0500

    Updated the mutation so that it works now.  The issue was most of the boilerplate that was there wasn't needed for a multitude of reasons. The options weren't needed since the parameters are named the same as the object name on the props.  The transform output data portion isn't really used on mutations and what was actually causing the issue

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -12,44 +12,46 @@ class LoginPage extends React.Component {
         submittedPassword: ''
     };
 
-    handleChange = (e, { name, value }) => {
-        this.setState({ [name]: value });
+    handleChange = (e, {name, value}) => {
+        this.setState({[name]: value});
     };
 
     handleSubmit = () => {
-        const { username, password } = this.state;
-
-        this.setState({ submittedUsername: username, submittedPassword: password });
-        console.log(this.state);
+        const {username, password} = this.state;
+        this.setState({submittedUsername: username, submittedPassword: password});
 
         let mutationVariables = {
             username: username,
             password: password
         };
-
         console.log(this.props);
-        this.props.createTokenMutation({variables: mutationVariables}).then((response) => {
+        this.props.createToken({variables: mutationVariables}).then((response) => {
             let token = response.data.createToken.token;
-            console.log(response);
-            //console.log(token);
+            console.log(token);
         }).catch((error) => {
-             console.log(error);
+            console.log("Caught an error");
+            if(error.message.includes("'NoneType' object has no attribute")){
+                //Incorrect login, give the user a heads up that the login details are wrong
+            }
+            console.log(error);
         });
     };
 
 
     render() {
-        const { username, password } = this.state;
+        const {username, password} = this.state;
         return (
             <div>
                 <Form onSubmit={this.handleSubmit}>
                     <Form.Field required>
                         <label>Username</label>
-                        <Form.Input width={4} placeholder='Username' name='username' value={username} onChange={this.handleChange}/>
+                        <Form.Input width={4} placeholder='Username' name='username' value={username}
+                                    onChange={this.handleChange}/>
                     </Form.Field>
                     <Form.Field required>
                         <label>Password</label>
-                        <Form.Input width={4} type='password' placeholder='Password' name='password' value={password} onChange={this.handleChange}/>
+                        <Form.Input width={4} type='password' placeholder='Password' name='password' value={password}
+                                    onChange={this.handleChange}/>
                     </Form.Field>
                     <Button type='submit'>Submit</Button>
                 </Form>
@@ -57,8 +59,6 @@ class LoginPage extends React.Component {
         );
     }
 }
-
-
 
 
 export default createToken(LoginPage);
--- src/queries/createToken.js
+++ src/queries/createToken.js
@@ -16,22 +16,5 @@ export const mutation = gql`
 `;
 
 export default compose(
-    graphql(mutation, {
-        // read query vars into query from input data above
-        options: (props) => ({
-            variables: {
-                username: props.username,
-                password: props.password
-            }
-        }),
-        // transform output data
-        props: ({ownProps}) => (
-            {
-                ownProps
-                // loading,
-                // error,
-                // refetchAgent: refetch,  // :NOTE: call this in the component to force reload the data
-                // organizationList: viewer ? viewer.token : null,
-            }), name:"createTokenMutation",
-    })
+    graphql(mutation, {name:"createToken"}),
 );
