099a5020f3c3edbb24661a41479db04ee2865ac3
larsonme <37116076+larsonme@users.noreply.github.com>
2019-03-21 08:44:36 -0500

    Update getMyAgent.tsx

--- src/queries/Agent/getMyAgent.tsx
+++ src/queries/Agent/getMyAgent.tsx
@@ -7,7 +7,7 @@
  */
 
 import { connect } from "react-redux";
-import { graphql, compose } from "react-apollo";
+import {graphql, compose} from "react-apollo";
 import gql from "graphql-tag";
 
 import { agentInterface } from "./agentInterface";
