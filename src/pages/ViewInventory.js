cec25aa0bc5b62948de3dccb2042945ce0cffd96
Donal Moloney <Moloneyda@msoe.edu>
2019-03-23 10:06:49 -0500

    Further worked on implemnting the inventory page, however keep getting graph ql not enough segments erro

--- src/pages/ViewInventory.js
+++ src/pages/ViewInventory.js
@@ -1,11 +1,59 @@
 import React from 'react';
+// import {Header} from 'semantic-ui-react'
 
 import 'semantic-ui-css/semantic.min.css'
+import getOrganizationById from "../queries/Organization/getOrganizationById.tsx";
+
+export const GetSingleOrganization = getOrganizationById(({ organization, loading, error }) => {
+
+    if (loading) {
+        return(
+            <strong>Loading...</strong>
+        );
+    } else if (error) {
+        console.log("AAAAAAAAAAAAAAA");
+        console.log(error);
+        console.log("AAAAAAAAAAAAAAA");
+
+        return(
+            <p style={{color: "#F00"}}>API error</p>
+        );
+    } else {
+        console.log("_________________________________");
+        console.log(organization);
+
+        return(
+            <div>
+                <Organization organization={organization}/>
+            </div>
+        );
+    }
+});
+
+export const Organization = (props) => {
+    let organization = props.organization;
+    return(
+        <div>
+            <div>id: {organization.id}</div>
+            <div>name: {organization.name}</div>
+            <div>type: {organization.type}</div>
+            <div>image: {organization.image}</div>
+            <div>note: {organization.note}</div>
+            {/*<div>ownedEconomicResources: {concatArray(organization.ownedEconomicResources)}</div>*/}
+            {/*<div>agentProcesses: {concatArray(organization.agentProcesses)}</div>*/}
+            {/*<div>agentPlans: {concatArray(organization.agentPlans)}</div>*/}
+            {/*<div>agentEconomicEvents: {concatArray(organization.agentEconomicEvents)}</div>*/}
+            {/*<div>agentCommitments: {concatArray(organization.agentCommitments)}</div>*/}
+            {/*<div>agentRelationships: {concatArray(organization.agentRelationships)}</div>*/}
+            {/*<div>agentRoles: {concatArray(organization.agentRoles)}</div>*/}
+            {/*<div>agentRecipess: {concatArray(organization.agentRecipes)}</div>*/}
+            <br/>
+        </div>
+    );
+};
 
 
 class ViewInventory extends React.Component {
-
-
 
     render() {
         //You need the two <br> tag so that the header does not cover up the text
@@ -13,7 +61,9 @@ class ViewInventory extends React.Component {
             <div>
                 <br/>
                 <br/>
-                It works
+                <h1 class="ui header">Your Inventory</h1>
+                <br/>
+                <GetSingleOrganization organizationId={8}/> :
             </div>
         )
     }
--- src/routes.js
+++ src/routes.js
@@ -4,16 +4,16 @@ import OrganizationCatalog from './pages/OrganizationCatalog.js';
 import BasicTextPage from './pages/BasicTextPage.js';
 import ErrorPage from './pages/ErrorPage.js';
 import LoginPage from './pages/LoginPage.js';
-import SearchResults from './pages/oldReaapp/organizationCatalog';
+import SearchResults from './pages/OrganizationCatalog';
 import ViewInventory from './pages/ViewInventory';
 import OrganizationRegistration from './pages/oldReaapp/organizationRegistration';
-import Header from './pages/Header.js';
+// import Header from './pages/Header.js';
 
 class Routes extends Component {
     render() {
         return (
             <div>
-                <Header/>
+                {/*<Header/>*/}
                 <Switch>
                     <Route exact path="/" component={OrganizationCatalog}/>
                     <Route exact path="/login" component={LoginPage}/>
