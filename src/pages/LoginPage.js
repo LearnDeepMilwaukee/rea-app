fdab346e2de3a73e82af15e7b1243eb1f6cefe38
Aaron Murphy <murphyad@msoe.edu>
2019-03-25 14:36:30 -0500

    Added verification of login and user notifcation, route changes on login success to site homepage. Currently does not render the new page, this is being looked into.

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import {Form, Button, Grid, Header} from 'semantic-ui-react'
+import {Form, Button, Grid, Header, Segment, Message} from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css'
 import createToken from "../queries/createToken.js";
 import * as currentUserActions from '../redux/actions/currentUserActions';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
+import { withRouter, Redirect} from 'react-router-dom';
+//import {COLOR_BUTTON_SUBMIT} from "../resources/colors.js";
 
 class LoginPage extends React.Component {
 
@@ -12,66 +14,80 @@ class LoginPage extends React.Component {
         username: '',
         password: '',
         submittedUsername: '',
-        submittedPassword: ''
+        submittedPassword: '',
+        loginFailed: false,
+        loginSuccess: false
     };
 
     handleChange = (e, {name, value}) => {
-        this.setState({[name]: value});
+    this.setState({[name]: value});
+};
+
+handleSubmit = () => {
+    const {username, password} = this.state;
+    this.setState({submittedUsername: username, submittedPassword: password});
+
+    let mutationVariables = {
+        username: username,
+        password: password
     };
-
-    handleSubmit = () => {
-        const {username, password} = this.state;
-        this.setState({submittedUsername: username, submittedPassword: password});
-
-        let mutationVariables = {
-            username: username,
-            password: password
-        };
-        console.log(this.props);
-        this.props.createToken({variables: mutationVariables}).then((response) => {
-            let token = response.data.createToken.token;
-            this.props.currentUserActions.setCurrentUserToken(token);
-            if(this.props.currentUserToken !== "N/A"){
-                // DISPLAY SUCCESS / REDIRECT
-            }
-            // console.log(token);
-        }).catch((error) => {
-            console.log("Caught an error");
-            if (error.message.includes("'NoneType' object has no attribute")) {
-                //Incorrect login, give the user a heads up that the login details are wrong
-            }
-            console.log(error);
-        });
-    };
-
-
-    render() {
-        const {username, password} = this.state;
-        return (
-            <div>
-                <Grid textAlign='center' style={{height: '100%'}} verticalAlign='middle' columns={1} centered>
-                    <Grid.Row>
-                        <Grid.Column style={{maxWidth: 450}}>
-                            <Header as='h2' textAlign='center'>
-                             Log-in to Makerspace
-                         </Header>
-                            <Form size='large' onSubmit={this.handleSubmit}>
-                                <Form.Field required>
-                                    <Form.Input fluid placeholder='Username' name='username' value={username}
-                                            onChange={this.handleChange}/>
-                                </Form.Field>
-                                <Form.Field required>
-                                    <Form.Input fluid type='password' placeholder='Password' name='password' value={password}
-                                            onChange={this.handleChange}/>
-                                </Form.Field>
-                                <Button type='submit'>Submit</Button>
-                            </Form>
-                        </Grid.Column>
-                    </Grid.Row>
-                </Grid>
-            </div>
-        );
+    console.log(this.props);
+    this.props.createToken({variables: mutationVariables}).then((response) => {
+        let token = response.data.createToken.token;
+    this.props.currentUserActions.setCurrentUserToken(token);
+    if(this.props.currentUserToken !== "N/A"){
+        this.setState({loginFailed: false});
+        // DISPLAY SUCCESS / REDIRECT
+        this.setState({loginSuccess: true});
     }
+    // console.log(token);
+}).catch((error) => {
+        console.log("Caught an error");
+    if (error.message.includes("'NoneType' object has no attribute")) {
+        //Incorrect login, give the user a heads up that the login details are wrong
+        this.setState({loginFailed: true});
+    }
+    console.log(error);
+});
+};
+
+renderRedirect = () => {
+    if (this.state.loginSuccess) {
+        return <Redirect to={'/'}/>;
+    }
+};
+
+render() {
+    const {username, password} = this.state;
+    return (
+        <div classname='login' >
+            {this.renderRedirect()}
+        <Grid textAlign='center' verticalAlign='middle' columns={1} centered>
+    <Grid.Row>
+    <Grid.Column style={{maxWidth: 450}}>
+<Header as='h2' textAlign='center'>
+        Log-in to Makerspace
+    </Header>
+    <Form size='large' onSubmit={this.handleSubmit} error={this.state.loginFailed}>
+<Segment stacked>
+    <Form.Field required>
+    <Form.Input fluid placeholder='Username' name='username' value={username}
+    onChange={this.handleChange}/>
+    </Form.Field>
+    <Form.Field required>
+    <Form.Input fluid type='password' placeholder='Password' name='password' value={password}
+    onChange={this.handleChange}/>
+    </Form.Field>
+    <Button color='blue' fluid type='submit' size='large'>Login</Button>
+        </Segment>
+        <Message error header='Login attempt failed!' list={["Please check your credentials!"]}/>
+    </Form>
+    </Grid.Column>
+    </Grid.Row>
+    </Grid>
+</div>
+);
+}
 }
 
 function mapStateToProps(state) {
@@ -87,7 +103,7 @@ function mapDispatchToProps(dispatch) {
     };
 }
 
-export default connect(
+export default withRouter(connect(
     mapStateToProps,
     mapDispatchToProps
-)(createToken(LoginPage));
+)(createToken(LoginPage)));
