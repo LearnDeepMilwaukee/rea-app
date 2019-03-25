2c00b39b9e1f3b94470c4f28bf6ed0cb6df335dc
Donal Moloney <Moloneyda@msoe.edu>
2019-03-25 09:42:52 -0500

    Have the resources being displayed on the screen. However, the ResourceClassified as does not work

--- src/pages/ViewInventory.js
+++ src/pages/ViewInventory.js
@@ -3,6 +3,9 @@ import React from 'react';
 
 import 'semantic-ui-css/semantic.min.css'
 import getOrganizationById from "../queries/Organization/getOrganizationById.tsx";
+import getEconomicResourceById from "../queries/EconomicResource/getEconomicResourceById.tsx";
+
+import { concatArray } from "./methods/common.tsx";
 
 export const GetSingleOrganization = getOrganizationById(({ organization, loading, error }) => {
 
@@ -11,49 +14,103 @@ export const GetSingleOrganization = getOrganizationById(({ organization, loadin
             <strong>Loading...</strong>
         );
     } else if (error) {
-        console.log("AAAAAAAAAAAAAAA");
         console.log(error);
-        console.log("AAAAAAAAAAAAAAA");
 
         return(
             <p style={{color: "#F00"}}>API error</p>
         );
     } else {
-        console.log("_________________________________");
-        console.log(organization);
+        // var ret_val = [];
+        // console.log(organization.ownedEconomicResources);
+        // let resource_count = organization.ownedEconomicResources.length;
+        // for(let i = 0; i<resource_count; i++) {
+        //     ret_val.push(<GetSingleEconomicResource economicResource={22}/>);
+        // }
+        // console.log("VALUES");
+        // console.log(ret_val);
+        let economicResourceList = organization.ownedEconomicResources;
+        console.log("_____________________");
+        console.log(economicResourceList);
+        console.log("- - - - - - - - -  - -  --  - -- - - ");
 
         return(
             <div>
-                <Organization organization={organization}/>
+                {economicResourceList.map( (economicResource) =>
+                    (<EconomicResource key={economicResource.id} economicResource={economicResource}/>)
+                )}
             </div>
         );
     }
 });
 
-export const Organization = (props) => {
-    let organization = props.organization;
+
+// export const OrganizationResources = (props) => {
+//     let organization = props.organization;
+//     return(
+//         <div>
+//             <div>id: {organization.id}</div>
+//             <div>name: {organization.name}</div>
+//             <div>type: {organization.type}</div>
+//             <div>image: {organization.image}</div>
+//             <div>note: {organization.note}</div>
+//             <div>ownedEconomicResources: {concatArray(organization.ownedEconomicResources)}</div>
+//             {/*<div>agentProcesses: {concatArray(organization.agentProcesses)}</div>*/}
+//             {/*<div>agentPlans: {concatArray(organization.agentPlans)}</div>*/}
+//             {/*<div>agentEconomicEvents: {concatArray(organization.agentEconomicEvents)}</div>*/}
+//             {/*<div>agentCommitments: {concatArray(organization.agentCommitments)}</div>*/}
+//             {/*<div>agentRelationships: {concatArray(organization.agentRelationships)}</div>*/}
+//             {/*<div>agentRoles: {concatArray(organization.agentRoles)}</div>*/}
+//             {/*<div>agentRecipess: {concatArray(organization.agentRecipes)}</div>*/}
+//             <br/>
+//         </div>
+//     );
+// };
+
+
+export const GetSingleEconomicResource = getEconomicResourceById(({ economicResource, loading, error }) => {
+
+    if (loading) {
+        return(
+            <strong>Loading...</strong>
+        );
+    } else if (error) {
+        return(
+            <p style={{color: "#F00"}}>API error</p>
+        );
+    } else {
+        return(
+            <div>
+                <EconomicResource economicResource={economicResource}/>
+            </div>
+        );
+    }
+});
+
+
+export const EconomicResource = (props) => {
+    let economicResource = props.economicResource;
+    console.log("_________________________________");
+
+    console.log(economicResource);
     return(
         <div>
-            <div>id: {organization.id}</div>
-            <div>name: {organization.name}</div>
-            <div>type: {organization.type}</div>
-            <div>image: {organization.image}</div>
-            <div>note: {organization.note}</div>
-            {/*<div>ownedEconomicResources: {concatArray(organization.ownedEconomicResources)}</div>*/}
-            {/*<div>agentProcesses: {concatArray(organization.agentProcesses)}</div>*/}
-            {/*<div>agentPlans: {concatArray(organization.agentPlans)}</div>*/}
-            {/*<div>agentEconomicEvents: {concatArray(organization.agentEconomicEvents)}</div>*/}
-            {/*<div>agentCommitments: {concatArray(organization.agentCommitments)}</div>*/}
-            {/*<div>agentRelationships: {concatArray(organization.agentRelationships)}</div>*/}
-            {/*<div>agentRoles: {concatArray(organization.agentRoles)}</div>*/}
-            {/*<div>agentRecipess: {concatArray(organization.agentRecipes)}</div>*/}
+            <div>Id: {economicResource.id}</div>
+            {/*<div>Resource Classification: {economicResource.resourceClassifiedAs.id}</div>*/}
+            <div>Tracking Id: {economicResource.trackingIdentifier}</div>
+            <div>Image: {economicResource.image}</div>
+            <div>currentQuantity: {economicResource.currentQuantity ? economicResource.currentQuantity.numericValue : "null"}
+                {economicResource.currentQuantity ? " " +  economicResource.currentQuantity.unit.name : ""}</div>
+            <div>Note: {economicResource.note}</div>
+            <div>Category: {economicResource.category}</div>
+            <div>Transfers: {concatArray(economicResource.transfers)}</div>
             <br/>
         </div>
     );
 };
 
-
 class ViewInventory extends React.Component {
+
+
 
     render() {
         //You need the two <br> tag so that the header does not cover up the text
