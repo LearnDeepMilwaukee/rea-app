37b5e5e16484b01f9c36187be97068968ee0aaf8
Aaron Murphy <murphyad@msoe.edu>
2019-04-11 09:18:37 -0500

    Converted inventory card from a const to a component so it can interact with the sites history to redirect the user when the edit item page is implemented

--- src/pages/Organizations/organizationInventory.js
+++ src/pages/Organizations/organizationInventory.js
@@ -9,88 +9,128 @@ import 'semantic-ui-css/semantic.min.css'
 import getOrganizationById from "../../queries/Organization/getOrganizationById.tsx";
 import getEconomicResourceById from "../../queries/EconomicResource/getEconomicResourceById.tsx";
 import {isNullOrUndefined} from "util"
-import {Item, Button, Loader} from 'semantic-ui-react'
+import {Item, Button, Loader, Form} from 'semantic-ui-react'
+import {withRouter} from "react-router-dom";
 
 let default_image = require("../../resources/default_resource_img.jpg");
 
 /**
- * Gets an organizations data
- */
-export const GetSingleOrganization = getOrganizationById(({ organization, loading, error }) => {
-    if (loading) {
-        return(
-            <Loader>Loading</Loader>
-        );
-    } else if (error) {
-        return(
-            <p style={{color: "#F00"}}>API error</p>
-        );
-    } else {
-        let economicResourceList = organization.ownedEconomicResources;
-        return(
-            <div>
-                <h2 className="ui header">{organization.name} Inventory</h2>
-                <Item.Group divided>
-                {(economicResourceList.length ===0)? <p>Inventory Empty</p> :(economicResourceList.map( (economicResource) =>
-                    (<GetSingleEconomicResource economicResourceId={economicResource.id}/>)
-                ))}
-                </Item.Group>
-            </div>
-        );
-    }
-});
-
-/**
- * Gets a single economic resource that the organization owns
- */
-export const GetSingleEconomicResource = getEconomicResourceById(({ economicResource, loading, error }) => {
-    if (loading) {
-        return(
-            <Loader>Loading</Loader>
-        );
-    } else if (error) {
-        return(
-            <p style={{color: "#F00"}}>API error</p>
-        );
-    } else {
-        return(
-                <EconomicResource economicResource={economicResource}/>
-        );
-    }
-});
-
-/**
  * Formats economicresource data
  */
-export const EconomicResource = (props) => {
-    let economicResource = props.economicResource;
-    return(
-        <Item classname={""}>
-            <Item.Image className={"ui small rounded image"} src={isNullOrUndefined(economicResource.image) || economicResource.image === "" ? default_image : economicResource.image} onError={i => i.target.src=default_image}/>
-            <Item.Content>
-                <Item.Header as='h1' >{economicResource.trackingIdentifier}</Item.Header>
-                <Item.Description>
-                    <p>{(economicResource.note === "") ? "(no description available)" : economicResource.note}</p>
-                    <p>Quantity: {economicResource.currentQuantity.numericValue} {economicResource.currentQuantity.unit.name}(s)</p>
-                    <p>Added on: {economicResource.createdDate}</p>
-                </Item.Description>
-                <Item.Extra>
-                    <Button className="ui right floated primary">Edit</Button>
-                </Item.Extra>
-            </Item.Content>
-        </Item>
-    );
-};
+class InventoryCard extends React.Component {
 
-class OrganizationInventory extends React.Component {
+    editItem = () => {
+        // TODO
+        // this.props.history.push("URL HERE");
+        // window.location.reload();
+    }
+
     render() {
         return (
-            <div className="ui container">
-                <GetSingleOrganization organizationId={this.props.match.params.id}/>
+            <Item classname={""}>
+                <Item.Image className={"ui small rounded image"}
+                            src={isNullOrUndefined(this.props.economicResource.image) || this.props.economicResource.image === "" ? default_image : this.props.economicResource.image}
+                            onError={i => i.target.src = default_image}/>
+                <Item.Content>
+                    <Item.Header as='h1'>{this.props.economicResource.trackingIdentifier}</Item.Header>
+                    <Item.Description>
+                        <p>{(this.props.economicResource.note === "") ? "(no description available)" : this.props.economicResource.note}</p>
+                        <p>
+                            Quantity: {this.props.economicResource.currentQuantity.numericValue} {this.props.economicResource.currentQuantity.unit.name}(s)</p>
+                        <p>Added on: {this.props.economicResource.createdDate}</p>
+                    </Item.Description>
+                    <Item.Extra>
+                        <Button className="ui right floated primary" onClick={this.editItem}>Edit</Button>
+                    </Item.Extra>
+                </Item.Content>
+            </Item>
+        );
+    }
+}
 
+const optionsSort = [
+    { key: 'alp', text: 'Alphabetical (A-Z)', value: 'alphabetical' },
+    { key: 'alprev', text: 'Alphabetical (Z-A)', value: 'alphabeticalreverse' },
+    { key: 'new', text: 'Date (Newest)', value: 'newest' },
+    { key: 'old', text: 'Date (Oldest)', value: 'oldest' }
+];
+
+class OrganizationInventory extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            sorting: "alphabetical",
+            typeFilter: "All",
+            distanceFilter: 50,
+            load: true,
+            nameFilter: "",
+        };
+
+        this.onSortFilterChange = this.onSortFilterChange.bind(this);
+    }
+
+
+    onSortFilterChange = function (event, {value}) {
+        this.setState({sorting: value});
+    };
+
+    render() {
+        const GetSingleEconomicResource = getEconomicResourceById(({ economicResource, loading, error }) => {
+            if (loading) {
+                return(
+                    <Loader>Loading</Loader>
+                );
+            } else if (error) {
+                return(
+                    <p style={{color: "#F00"}}>API error</p>
+                );
+            } else {
+                return(
+                    <InventoryCard economicResource={economicResource} history={this.props.history}/>
+                );
+            }
+        });
+
+        const OrgInventory = getOrganizationById(({ organization, loading, error }) => {
+            if (loading) {
+                return(
+                    <Loader>Loading</Loader>
+                );
+            } else if (error) {
+                return(
+                    <p style={{color: "#F00"}}>API error</p>
+                );
+            } else {
+                let economicResourceList = organization.ownedEconomicResources;
+                return(
+                    <div>
+                        <h2 className="ui header">{organization.name} Inventory</h2>
+                        <Form>
+                            <Form.Group widths='equal'>
+                                <Form.Input fluid label='Search For Org' placeholder='Search' />
+                                <Form.Select fluid label='Sort' options={optionsSort} value={this.state.sorting} onChange={this.onSortFilterChange}/>
+                            </Form.Group>
+                            <Form.Group inline>
+                                <Form.Button className="ui negative" color={'red'} onClick={this.handleReset}>Reset filters</Form.Button>
+                            </Form.Group>
+                        </Form>
+                        <Item.Group divided>
+                            {(economicResourceList.length ===0)? <p>Inventory Empty</p> :(economicResourceList.map( (economicResource) =>
+                                (<GetSingleEconomicResource economicResourceId={economicResource.id}/>)
+                            ))}
+                        </Item.Group>
+                    </div>
+                );
+            }
+        });
+
+        return (
+            <div className="ui container">
+                <OrgInventory organizationId={this.props.match.params.id}/>
             </div>
         )
     }
 }
 
-export default OrganizationInventory;
+export default withRouter(OrganizationInventory);
